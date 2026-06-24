943585cd7b70223f3a80486b2bd816cfe1a6c65d
Clifford Addo <cliffordaddo21@gmail.com>
2026-06-23 23:47:59 -0400

    Working dashboard connected to Flask

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello, World!"
+def home():
+    return render_template('index.html')
 
 @app.route('/submit', methods=["POST"])
 def submit():
